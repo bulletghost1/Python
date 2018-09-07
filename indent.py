b730fec9bb5da2900bb4124456216b6efd478434
bulletghost1 <42982213+bulletghost1@users.noreply.github.com>
2018-09-07 12:45:06 -0700

    Add files via upload

---
+++ indent.py
@@ -0,0 +1,10 @@
+# indent.py jb
+
+print("hello world")
+if (1 > 0):
+	print("greater than")
+	print(">")
+if (1 > 0):
+	print("less than")
+	print("<")
+
---
+++ vars.py
@@ -0,0 +1,6 @@
+# vars.py jb
+print(" pi, e, and product are the variables .")
+pi = 3.1415926535
+e = 2.7182818284590
+product = pi ** e
+print(pi, e, product)
